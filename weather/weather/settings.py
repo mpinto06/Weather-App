e7d1aa45663cd056d3bdd1d82efafb62ec11462e
Miguel  Pinto <miguelep0106@gmail.com>
2023-12-31 18:39:44 -0400

    fix: fixed root name

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'images/'
-STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
