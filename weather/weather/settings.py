65d57babe8a3a1b26d518f8e712779e41fcf3469
Miguel  Pinto <miguelep0106@gmail.com>
2023-12-31 18:52:20 -0400

    fix: aaaaa

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = ''
 MEDIA_URL = 'images/'
 STATIC_ROOT  = BASE_DIR / 'staticfiles'
 
