34fdb59b915b0246e77aa2e8efc9820d03dc4d92
Miguel  Pinto <miguelep0106@gmail.com>
2023-12-31 19:27:19 -0400

    aaa

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -125,11 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = 'images/'
-STATIC_ROOT  = BASE_DIR / 'staticfiles'
+MEDIA_URL = '/images/'
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    [os.path.join(BASE_DIR, 'static')]
 ]
 
 # Default primary key field type
